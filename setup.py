4b4210ad56ba3cd981de2e1deff9bf60968ab8b1
Benjamin Morgan <benmorgan2@gmail.com>
2018-03-20 22:39:46 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+readme = 'README.md'
+try:
+    import pypandoc
+    long_description = pypandoc.convert( readme, 'rst')
+except ImportError:
+    long_description = open( readme ).read()
+
+from polyhedral_analysis import __version__ as VERSION
+
+config = {
+    'name': 'polyhedral_analysis',
+    'description': 'TODO'
+    'long_description': long_description,
+    'author': 'Benjamin J. Morgan',
+    'author_email':'bjm42@bath.ac.uk',
+    'url': 'https://github.com/bjmorgan/polyhedral-analysis', 
+    'download_url': "https://github.com/bjmorgan/polyhedral-analysis/archive/%s.tar.gz" % (VERSION),
+    'version': VERSION,
+    'packages': ['polyhedral_analysis'],
+    'license': 'MIT',
+    'install_requires': [ 'numpy',
+                          'pymatgen',
+                          'scipy', 
+                          'coverage==4.3.4',
+                          'codeclimate-test-reporter' ]
+}
+
+setup(**config)
