f4a430c2a9e76c30e39524ad951919134111ce34
Benjamin Morgan <benmorgan2@gmail.com>
2018-03-21 22:16:33 +0000

    Updated Atom class

--- polyhedral_analysis/atom.py
+++ polyhedral_analysis/atom.py
@@ -6,14 +6,15 @@ class Atom:
         Initialise an Atom object.
 
         Args:
-            index (int): numerical index identifying this atom.
+            index (int): Numerical index identifying this atom.
             site  (pymatgen.Site): The pymatgen Site (or PeriodicSite) object describing this atom.
             label (:obj:`str`, optional): An optional string labelling this atom.
 
         Attributes:
-            in_polyhedra (list): TODO
-            neighbours: (None|list): TODO
-
+            in_polyhedra (list): List of polyhedra that this atom is part of.
+            neighbours: (None|dict(list)): Dictionary of lists of neighbours for each 
+                polyhedron this atom is part of. Dictionary keys are the indexes of 
+                each respective polyhedron.
         """
         self.index = index
         self.site = site
@@ -24,12 +25,12 @@ class Atom:
     def __repr__( self ):
         return "{} {}".format( self.index, self.site )
 
-    def construct_neighbour_list( self, structure, cutoff, vertex_atoms, poly_index ):
-        vertex_indices = [ v.index for v in vertex_atoms ]
-        if not self.neighbours:
-            self.neighbours = {}
-        self.neighbours[ poly_index ] = []
-        self.neighbours[ poly_index ] = [ v.index for v in vertex_atoms if self.site.distance( v.site ) < cutoff and v is not self ]
+#    def construct_neighbour_list( self, cutoff, vertex_atoms, poly_index ):
+#        vertex_indices = [ v.index for v in vertex_atoms ]
+#        if not self.neighbours:
+#            self.neighbours = {}
+#        self.neighbours[ poly_index ] = []
+#        self.neighbours[ poly_index ] = [ v.index for v in vertex_atoms if self.site.distance( v.site ) < cutoff and v is not self ]
 
     @property
     def frac_coords( self ):
