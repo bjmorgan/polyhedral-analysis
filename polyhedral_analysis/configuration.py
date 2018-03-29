0e3bc49a78e2d55e3e2a4d76c089f95d685c2472
Benjamin Morgan <benmorgan2@gmail.com>
2018-03-29 23:14:37 +0100

    Added atom lists to Configuration objects

--- polyhedral_analysis/configuration.py
+++ polyhedral_analysis/configuration.py
@@ -1,5 +1,8 @@
 from .atom import Atom
 
+def flatten( this_list ):
+    return [ item for sublist in this_list for item in sublist ]
+
 class Configuration:
 
     def __init__( self, structure, recipes, config_number=None ):
@@ -10,11 +13,17 @@ class Configuration:
         self.polyhedra = []
         for recipe in recipes:
             self.polyhedra.extend( recipe.find_polyhedra( self.atoms, self.structure ) )
-                                                           
+        self.central_atoms = sorted( list( set( 
+                                 [ p.central_atom for p in self.polyhedra ] ) ) )
+        self.coordination_atoms = sorted( list( set( flatten( 
+                                      [ p.vertices for p in self.polyhedra ] ) ) ) )
+
+    def coordination_atoms_by_index( self, index ):
+        return next( atom for atom in self.coordination_atoms if atom.index == index )
+
     @property
     def polyhedra_labels( self ):
         return [ p.label for p in self.polyhedra ]
-
 
     def polyhedra_by_label( self, label ):
         return [ p for p in self.polyhedra if p.label == label ]
