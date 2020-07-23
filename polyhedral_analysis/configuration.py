d358f059ae10c2ab8f7f896b2a0a2d18f4a655eb
Benjamin Morgan <benmorgan2@gmail.com>
2020-07-23 11:12:19 +0100

    Added Configuration tests

--- polyhedral_analysis/configuration.py
+++ polyhedral_analysis/configuration.py
@@ -7,8 +7,8 @@ from typing import Optional, List, Union
 
 class Configuration:
 
-    def __init__(self, 
-                 structure: Structure, 
+    def __init__(self,
+                 structure: Structure,
                  recipes: List[PolyhedraRecipe]) -> None:
         """
         A Configuration object describes a single atomic geometry.
@@ -17,7 +17,7 @@ class Configuration:
             structure (pymatgen.Structure): A pymatgen Structure object for this configuration.
             recipes (list(PolyhedraRecipe): A list of PolyhedraRecipe objects used to construct
                 polyhedra for this configuration.
- 
+
         Attributes:
             atoms (list(Atom)): A list of atoms that make up this configuration.
             polyhedra (list(CoordinationPolyhedron): A list of polyhedra, generated using
@@ -27,19 +27,19 @@ class Configuration:
             coordination_atoms (list(Atom)): A list of atoms that define the vertices of 
                 the coordination polyhedra.
          """
-        self.atoms = [Atom(index=i, 
-                           site=site, 
-                           label=site.species_string) 
+        self.atoms = [Atom(index=i,
+                           site=site,
+                           label=site.species_string)
                       for i, site in enumerate(structure.sites)]
         self.polyhedra: List[CoordinationPolyhedron] = []
         for recipe in recipes:
             self.polyhedra.extend(recipe.find_polyhedra(self.atoms, structure))
         self.central_atoms = sorted(list(set(
-                                 [p.central_atom for p in self.polyhedra])),
-                                 key=lambda x: x.index)
+            [p.central_atom for p in self.polyhedra])),
+            key=lambda x: x.index)
         self.coordination_atoms = sorted(list(set(flatten(
-                                      [p.vertices for p in self.polyhedra]))),
-                                      key=lambda x: x.index)
+            [p.vertices for p in self.polyhedra]))),
+            key=lambda x: x.index)
 
     def coordination_atom_by_index(self,
                                    index: int) -> Union[Atom, None]:
@@ -53,11 +53,12 @@ class Configuration:
             (Atom|None): The matching coordination atom. If the desired index does not match
                          any of the coordination atoms for this configuration, None is returned.
         """
-        coordination_atom_indices = [atom.index for atom in self.coordination_atoms]
+        coordination_atom_indices = [
+            atom.index for atom in self.coordination_atoms]
         if index not in coordination_atom_indices:
             return None
         else:
-            return self.coordination_atoms[coordination_atom_indices.index(index)] 
+            return self.coordination_atoms[coordination_atom_indices.index(index)]
 
     @property
     def polyhedra_labels(self) -> List[Union[str, None]]:
--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -6,61 +6,78 @@ from polyhedral_analysis.polyhedra_recipe import PolyhedraRecipe
 from polyhedral_analysis.coordination_polyhedron import CoordinationPolyhedron
 from polyhedral_analysis.atom import Atom
 
-class TestConfigurationInit( unittest.TestCase ):
+class TestConfigurationInit(unittest.TestCase):
 
-    def test_configuration_is_initialised( self ):
-        mock_sites = [ Mock( spec=Site ) for i in range(4) ]
-        for m, s in zip( mock_sites, [ 'A', 'B', 'C', 'D' ] ):
+    def test_configuration_is_initialised(self):
+        mock_sites = [Mock(spec=Site) for i in range(4)]
+        for m, s in zip(mock_sites, ['A', 'B', 'C', 'D']):
             m.species_string = s
-        mock_structure = Mock( spec=Structure )
+        mock_structure = Mock(spec=Structure)
         mock_structure.sites = mock_sites
-        mock_polyhedra = [ Mock( spec=CoordinationPolyhedron ) for i in range(2) ]
-        mock_atoms = [ Mock( spec=Atom ) for i in range(4) ]
-        for i, m in enumerate( mock_atoms ):
+        mock_polyhedra = [Mock(spec=CoordinationPolyhedron) for i in range(2)]
+        mock_atoms = [Mock(spec=Atom) for i in range(4)]
+        for i, m in enumerate(mock_atoms):
             m.index = i
         mock_polyhedra[0].central_atom = mock_atoms[0]
-        mock_polyhedra[0].vertices = [ mock_atoms[1] ]
+        mock_polyhedra[0].vertices = [mock_atoms[1]]
         mock_polyhedra[1].central_atom = mock_atoms[2]
-        mock_polyhedra[1].vertices = [ mock_atoms[3] ]
-        mock_recipe = Mock( spec=PolyhedraRecipe )
-        mock_recipe.find_polyhedra = Mock( return_value=mock_polyhedra )
-        with patch( 'polyhedral_analysis.configuration.Atom' ) as mock_Atom:
+        mock_polyhedra[1].vertices = [mock_atoms[3]]
+        mock_recipe = Mock(spec=PolyhedraRecipe)
+        mock_recipe.find_polyhedra = Mock(return_value=mock_polyhedra)
+        with patch('polyhedral_analysis.configuration.Atom') as mock_Atom:
             mock_Atom.side_effect = mock_atoms
-            configuration = Configuration( structure=mock_structure, recipes=[ mock_recipe ] )
+            configuration = Configuration(
+                structure=mock_structure, recipes=[mock_recipe])
         self.assertEqual(configuration.polyhedra, mock_polyhedra)
-        self.assertEqual(configuration.central_atoms, [mock_atoms[0], mock_atoms[2]])
-        self.assertEqual(configuration.coordination_atoms, [ mock_atoms[1], mock_atoms[3]])
+        self.assertEqual(configuration.central_atoms, [
+                         mock_atoms[0], mock_atoms[2]])
+        self.assertEqual(configuration.coordination_atoms,
+                         [mock_atoms[1], mock_atoms[3]])
         self.assertEqual(configuration.atoms, mock_atoms)
 
 class TestConfiguration(unittest.TestCase):
 
-    def setUp( self ):
-        mock_sites = [ Mock( spec=Site ) for i in range(4) ]
-        for m, s in zip( mock_sites, [ 'A', 'B', 'C', 'D' ] ):
+    def setUp(self):
+        mock_sites = [Mock(spec=Site) for i in range(4)]
+        for m, s in zip(mock_sites, ['A', 'B', 'C', 'D']):
             m.species_string = s
-        mock_structure = Mock( spec=Structure )
+        mock_structure = Mock(spec=Structure)
         mock_structure.sites = mock_sites
-        mock_polyhedra = [ Mock( spec=CoordinationPolyhedron ) for i in range(2) ]
-        mock_atoms = [ Mock( spec=Atom ) for i in range(4) ]
-        for i, m in enumerate( mock_atoms ):
+        mock_polyhedra = [Mock(spec=CoordinationPolyhedron) for i in range(2)]
+        mock_atoms = [Mock(spec=Atom) for i in range(4)]
+        for i, m in enumerate(mock_atoms):
             m.index = i
         mock_polyhedra[0].central_atom = mock_atoms[0]
-        mock_polyhedra[0].vertices = [ mock_atoms[1] ]
+        mock_polyhedra[0].vertices = [mock_atoms[1]]
+        mock_polyhedra[0].label = 'foo'
         mock_polyhedra[1].central_atom = mock_atoms[2]
-        mock_polyhedra[1].vertices = [ mock_atoms[3] ]
-        mock_recipe = Mock( spec=PolyhedraRecipe )
-        mock_recipe.find_polyhedra = Mock( return_value=mock_polyhedra )
-        with patch( 'polyhedral_analysis.configuration.Atom' ) as mock_Atom:
+        mock_polyhedra[1].vertices = [mock_atoms[3]]
+        mock_polyhedra[1].label = 'bar'
+        mock_recipe = Mock(spec=PolyhedraRecipe)
+        mock_recipe.find_polyhedra = Mock(return_value=mock_polyhedra)
+        with patch('polyhedral_analysis.configuration.Atom') as mock_Atom:
             mock_Atom.side_effect = mock_atoms
-            self.configuration = Configuration( structure=mock_structure, recipes=[ mock_recipe ] )
+            self.configuration = Configuration(
+                structure=mock_structure, recipes=[mock_recipe])
         self.mock_atoms = mock_atoms
 
-    def test_coordination_atom_by_index( self ):
-        self.assertEqual( self.configuration.coordination_atom_by_index( 3 ), self.mock_atoms[3] )
+    def test_coordination_atom_by_index(self):
+        self.assertEqual(
+            self.configuration.coordination_atom_by_index(3), self.mock_atoms[3])
 
-    def test_coordination_atom_by_index_returns_none_if_missing( self ):
-        self.assertEqual( self.configuration.coordination_atom_by_index(2 ), None )        
+    def test_coordination_atom_by_index_returns_none_if_missing(self):
+        self.assertEqual(
+            self.configuration.coordination_atom_by_index(2), None)
+
+    def test_polyhedra_labels(self):
+        self.assertEqual(self.configuration.polyhedra_labels, ['foo', 'bar'])
+
+    def test_polyhedra_by_label(self):
+        p = self.configuration.polyhedra_by_label('foo')
+        self.assertEqual(len(p), 1)
+        self.assertEqual(p[0].label, 'foo')
+        self.assertEqual(p[0].central_atom.index, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
-
