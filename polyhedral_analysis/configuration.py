4c4eb5991666f2f6e6303bbb39c0c67c7b6706b0
Benjamin Morgan <benmorgan2@gmail.com>
2020-07-25 16:16:01 +0100

    Added lattice_mc utilities

--- polyhedral_analysis/configuration.py
+++ polyhedral_analysis/configuration.py
@@ -1,9 +1,9 @@
 from polyhedral_analysis.atom import Atom
-from polyhedral_analysis.utils import flatten
+from polyhedral_analysis.utils import flatten, prune_neighbour_list, lattice_mc_string
 from pymatgen.core.structure import Structure
 from polyhedral_analysis.polyhedra_recipe import PolyhedraRecipe
 from polyhedral_analysis.coordination_polyhedron import CoordinationPolyhedron
-from typing import Optional, List, Union, Dict
+from typing import List, Union, Dict, Tuple
 
 class Configuration:
 
@@ -83,10 +83,20 @@ class Configuration:
         else:
             raise TypeError('Invalid type for label argument')
 
-    def neighbour_list(self,
-                       label1: Optional[str] = None,
-                       label2: Optional[str] = None) -> Dict[int, List[int]]:
-        """
-        TODO
-        """
-        pass 
+    def to_lattice_mc(self, 
+                      filename: str, 
+                      labels: List[str],
+                      neighbour_list: Dict[int, Tuple[int, ...]]) -> None:
+        site_indices = [p.index for p in self.polyhedra_by_label(labels)]
+        neighbour_list = prune_neighbour_list(neighbour_list, site_indices)
+        with open(filename, 'w') as f:
+            n_sites = len(self.polyhedra_by_label(labels))
+            f.write(f'{n_sites}\n\n')
+            for l in labels:
+                for p in self.polyhedra_by_label(l):
+                    f.write(f'{lattice_mc_string(p, neighbour_list)}\n')
+
+    def face_sharing_neighbour_list(self, 
+                                    labels: List[str]) -> Dict[int, Tuple[int, ...]]:
+        return {p.index: p.face_sharing_neighbour_list()
+                for p in self.polyhedra_by_label(labels)}
--- polyhedral_analysis/utils.py
+++ polyhedral_analysis/utils.py
@@ -1,4 +1,4 @@
-from typing import List, TypeVar, Sequence, Dict
+from typing import List, TypeVar, Sequence, Dict, Tuple
 from polyhedral_analysis.coordination_polyhedron import CoordinationPolyhedron
 
 T = TypeVar('T')
@@ -22,10 +22,20 @@ def flatten(this_list: Sequence[Sequence[T]]) -> List[T]:
 
 
 def lattice_mc_string(polyhedron: CoordinationPolyhedron,
-                      neighbour_list: Dict[int, List[int]]):
+                      neighbour_list: Dict[int, Tuple[int, ...]]):
     """TODO"""
     string = f'site: {polyhedron.index}\n'
     string += f'centre: {" ".join(f"{c:.8f}" for c in polyhedron.central_atom.coords)}\n'
     string += f'neighbours: {" ".join(str(i) for i in neighbour_list[polyhedron.index])}\n'
     string += f'label: {polyhedron.label}\n'
     return string
+
+
+def prune_neighbour_list(neighbours: Dict[int, Tuple[int, ...]], 
+                         indices: List[int]) -> Dict[int, Tuple[int, ...]]:
+    """TODO"""
+    pruned_neighbours = {}
+    for k, v in neighbours.items():
+        if k in indices:
+            pruned_neighbours[k] = tuple(set(v).intersection(set(indices)))
+    return pruned_neighbours
