6d7bdadab42fc11388cc70abd64388350867e945
Benjamin Morgan <benmorgan2@gmail.com>
2020-07-28 16:21:42 +0100

    Minor edit to make code clearer

--- polyhedral_analysis/octahedral_analysis.py
+++ polyhedral_analysis/octahedral_analysis.py
@@ -47,9 +47,9 @@ def opposite_vertex_pairs(polyhedron: CoordinationPolyhedron,
     vertex_pairs = []
     seen_indices: List[int] = []
     for v1 in polyhedron.vertices:
-        v1_neighbours = polyhedron.vertices_by_indices(v1.neighbours[polyhedron.index])
         if v1.index in seen_indices:
             continue
+        v1_neighbours = polyhedron.vertices_by_indices(v1.neighbours[polyhedron.index])
         v2 = next(v for v in polyhedron.vertices if v not in [v1, *v1_neighbours])
         vertex_pairs.append((v1, v2))
         seen_indices.extend([v1.index, v2.index])
