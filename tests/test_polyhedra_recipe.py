b15fedca889c3410e66dd28290c84d37ed14ff46
Benjamin Morgan <benmorgan2@gmail.com>
2024-07-01 18:05:19 +0100

    More unit tests

--- tests/test_polyhedra_recipe.py
+++ tests/test_polyhedra_recipe.py
@@ -2,7 +2,11 @@ import unittest
 import numpy as np
 from polyhedral_analysis.polyhedra_recipe import (PolyhedraRecipe,
                                                   matching_sites,
-                                                  polyhedra_from_distance_cutoff)
+                                                  polyhedra_from_distance_cutoff,
+                                                  generator_from_atom_argument,
+                                                  polyhedra_from_nearest_neighbours,
+                                                  polyhedra_from_closest_centre)
+from polyhedral_analysis.coordination_polyhedron import CoordinationPolyhedron
 from pymatgen.core import Structure, Lattice
 from polyhedral_analysis.atom import Atom
 from unittest.mock import Mock, patch
@@ -79,11 +83,74 @@ class TestPolyhedraRecipeFunctions(unittest.TestCase):
         central_atoms = []
         vertex_atoms = [ Mock(spec=Atom), Mock(spec=Atom) ]
         cutoff = 1.0
-        polyhedra = polyhedra_from_distance_cutoff( central_atoms=central_atoms,
-                                                    vertex_atoms=vertex_atoms,
-                                                    cutoff=cutoff )
-        self.assertEqual( polyhedra, [] )
+        polyhedra = polyhedra_from_distance_cutoff(central_atoms=central_atoms,
+                                                   vertex_atoms=vertex_atoms,
+                                                   cutoff=cutoff)
+        self.assertEqual(polyhedra, [])
+        
+class TestPolyhedraRecipe(unittest.TestCase):
+    
+    def setUp(self):
+        self.mock_structure = Mock(spec=Structure)
+        self.recipe = PolyhedraRecipe(method='distance cutoff',
+                                      central_atoms='Li',
+                                      vertex_atoms=['O', 'F'],
+                                      coordination_cutoff=2.5)
+                                      
+    def test_central_atom_list(self):
+        mock_generator = Mock(return_value=[0, 1, 2])
+        self.recipe._central_atom_list_generator = mock_generator        
+        result = self.recipe.central_atom_list(self.mock_structure)        
+        self.assertEqual(result, [0, 1, 2])
+        mock_generator.assert_called_once_with(self.mock_structure)
 
+    def test_vertex_atom_list(self):
+        mock_generator = Mock(return_value=[3, 4, 5])
+        self.recipe._vertex_atom_list_generator = mock_generator        
+        result = self.recipe.vertex_atom_list(self.mock_structure)        
+        self.assertEqual(result, [3, 4, 5])
+        mock_generator.assert_called_once_with(self.mock_structure)
+
+    def test_central_atom_list_no_recalculate(self):
+        self.recipe.recalculate = False
+        self.recipe._central_atom_list = [0, 1, 2]        
+        result = self.recipe.central_atom_list()        
+        self.assertEqual(result, [0, 1, 2])
+    
+    def test_find_polyhedra(self):
+        mock_atoms = [Mock(spec=Atom, index=i) for i in range(6)]
+        self.recipe.central_atom_list = Mock(return_value=[0, 1])
+        self.recipe.vertex_atom_list = Mock(return_value=[2, 3, 4, 5])
+        
+        with patch('polyhedral_analysis.polyhedra_recipe.polyhedra_from_distance_cutoff') as mock_polyhedra_func:
+            mock_polyhedra_func.return_value = [Mock(spec=CoordinationPolyhedron)]
+            
+            result = self.recipe.find_polyhedra(mock_atoms, self.mock_structure)
+            
+            mock_polyhedra_func.assert_called_once_with(
+                central_atoms=[mock_atoms[0], mock_atoms[1]],
+                vertex_atoms=[mock_atoms[2], mock_atoms[3], mock_atoms[4], mock_atoms[5]],
+                cutoff=2.5,
+                label=None
+            )
+            self.assertEqual(result, mock_polyhedra_func.return_value)
+
+class TestPolyhedraGenerationFunctions(unittest.TestCase):
+    def setUp(self):
+        self.mock_central_atoms = [Mock(spec=Atom) for _ in range(2)]
+        self.mock_vertex_atoms = [Mock(spec=Atom) for _ in range(4)]
+
+        # Assign coordinates to atoms
+        for i, atom in enumerate(self.mock_central_atoms + self.mock_vertex_atoms):
+            atom.coords = np.array([i, i, i], dtype=float)
+
+        # Implement distance method for mock atoms
+        def distance(self, other):
+            return np.linalg.norm(self.coords - other.coords)
+
+        for atom in self.mock_central_atoms + self.mock_vertex_atoms:
+            atom.distance = distance.__get__(atom)
+            
 if __name__ == '__main__':
     unittest.main()
 
